c2ba24f96bf1711825324cf940140166156e397d
medagli <Stefano.Medagli@rohde-schwarz.com>
2020-06-02 17:14:15 +0200

    add utilities module

---
+++ utils/common.py
@@ -0,0 +1,9 @@
+nr_country_to_show = 20
+country_order = ['USA', 'India', 'France', 'China', 'Russia', 'Japan', 'Ukraine', 'Germany', 'Italy', 'Spain',
+                 'Argentina', 'Brazil', 'United Kingdom', 'Mexico', 'South Africa', 'Canada', 'Indonesia', 'Pakistan',
+                  'Nigeria', 'Australia',
+                 'elsewhere',
+                 ]
+
+numeric_fill = -1 # value used to fill missing numeric values
+string_fill = ' ' # value used to fill missing non numeric values
---
+++ utils/dataframe.py
@@ -0,0 +1,26 @@
+"""
+A collection of quick functions for common operations of pandas' DataFrame
+"""
+import pandas as pd
+
+def get_number_of_na(df: pd.DataFrame) -> pd.Series:
+    """ Returns the number of nan for each column
+    Args:
+        df:
+    Returns:
+    """
+    return df.isna().sum()
+
+
+def get_year_stats(df: pd.DataFrame) -> pd.DataFrame:
+    """ Returns nr of entries and sum of masses per each year.
+    Args:
+        df: dataframe with columns ['year', 'name', 'mass']
+    Returns:
+        dataframe with columns ['nr', 'mass']
+    """
+    yearly = df.groupby('year')
+    d = pd.DataFrame()
+    d['nr'] = yearly.count().name
+    d['mass'] = yearly.sum()['mass']
+    return d
